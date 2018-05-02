cc90b2bbdf13259add4b24201aee888f89b9fb96
Confidence Okoghenun <confidenceboi@gmail.com>
2018-05-02 09:08:34 +0100

    Dynamic Grid and Color Selection
    This commit includes changes that allows users
    to create any number of grid and also to
    color each cell individually.
    
    *Functions 'createGrid' and 'colorGrid' were
     added to create and color the grids respectively.
    *This change has made the app more playable.
    *Next feature to work on would be bug fixes,
     preforamnce improvement and design.
    
    [Closes-Bug: #002]

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let createButton = document.querySelector("#create-grid");
+createButton.addEventListener("click", createGrid);
+
+function createGrid() {
+  if(document.querySelector("table")) {
+  document.querySelector("table").remove();
+  }
+  let gridWidth = document.querySelector("#grid-width").value;
+  let gridHeight = document.querySelector("#grid-height").value;
+  document.querySelector("#picker-control").reset();
+  if(((gridHeight === "")||(gridWidth === ""))) {
+    alert("You didn't enter the grid!");
+  } else if((gridHeight === "0")||(gridWidth === "0")) {
+    alert("Hey you entered an invalid grid!");
+  } else {
+    let paintArea = document.querySelector("#paint-area");
+    let table = document.createElement("table");
+    let row, col;
+    for(let i = 0; i < gridHeight; i++) {
+    row = document.createElement("tr");
+      for(let m = 0; m < gridWidth; m++) {
+        col = document.createElement("td");
+        row.appendChild(col);
+      }
+      table.appendChild(row);
+    }
+    paintArea.appendChild(table);
+    let cells = document.querySelectorAll("td");
+    for(let n = 0; n < cells.length; n++) {
+      cells[n].addEventListener("click", colorGrid);
+    }
+  }
+}
+function colorGrid(){
+  let color = document.querySelector("#color").value;
+  this.style.background = color;
+}
